fed692c232066068f270722f1c937f6fbfb9fbe1
ArteMaxL <artemiolucero@gmail.com>
2022-03-22 02:07:49 -0300

    Initial commit

---
+++ src/test/java/com/artedatamax/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artedatamax.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
